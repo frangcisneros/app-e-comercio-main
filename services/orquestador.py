ad69bed951b99879fe692e6eb2ddddb3f9191fcf
Francisco Cisneros <franbichir@gmail.com>
2024-10-22 13:47:38 -0300

    orquestador funcionando y compensaciones andando, test para probar compensar cuando falla el ultimo paso

--- services/orquestador.py
+++ services/orquestador.py
@@ -1,46 +1,80 @@
 from saga import Saga, SagaBuilder, SagaError
 import requests
+import json
 
-CATALOGO_SERVICE_URL = 'http://localhost:5003/api/v1/get_product/'
-COMPRA_SERVICE_URL = 'http://localhost:5001/compras/'
-PAGO_SERVICE_URL = 'http://localhost:5002/api/v1/pago'
-INVENTARIO_SERVICE_URL = 'http://localhost:5000/api/v1/stock/sell'
-ROLLBACK_CATALOGO_URL = 'http://inventory_service/rollback'
-ROLLBACK_PAYMENT_URL = 'http://payment_service/rollback'
 
-def crear_orden(data):
+def crear_compra(data):
+    r = requests.post("http://localhost:5001/compras/", json=data)
+    if r.status_code != 201:
+        raise Exception("Error al crear la compra")
+    else:
+        data["compra_id"] = r.json()["id"]
+        print(r.json())
+
+
+def compensar_compra(compra_id):
+    r = requests.delete(f"http://localhost:5001/compras/{compra_id}")
+    if r.status_code != 200:
+        raise Exception("Error al compensar la compra")
+    else:
+        print(r.json())
+
+
+def crear_pago(data):
+    r = requests.post("http://localhost:5002/api/v1/pago", json=data)
+    if r.status_code != 201:
+        raise Exception("Error al crear el pago")
+    else:
+        data["pago_id"] = r.json()["id"]
+        print(r.json())
+
+
+def compensar_pago(pago_id):
+    r = requests.delete(f"http://localhost:5002/api/v1/pago/eliminar/{pago_id}")
+    if r.status_code != 200:
+        raise Exception("Error al compensar el pago")
+    else:
+        print(r.json())
+
+
+def inventory_sell(data):
+    r = requests.post("http://localhost:5000/api/v1/stock/sell", json=data)
+    if r.status_code != 200:
+        print(r.json())
+        raise Exception("Error al vender el producto")
+    else:
+        print(r.json())
+
+
+def compensar_inventory_sell(data):
+    r = requests.post("http://localhost:5000/api/v1/stock/refuel", json=data)
+    if r.status_code != 200:
+        raise Exception("Error al compensar la venta del producto")
+    else:
+        print(r.json())
+
+
+def saga_compra(data):
     saga_builder = SagaBuilder.create()
-
-    # Paso : select producto
-    saga_builder.action(
-        action=lambda: requests.post(CATALOGO_SERVICE_URL, json=data),
-        compensation=lambda: requests.post(ROLLBACK_CATALOGO_URL, json=data)
-    )
-
     # Paso : Compra
     saga_builder.action(
-        action=lambda: requests.post(COMPRA_SERVICE_URL, json=data),
-        compensation=lambda: requests.post(COMPRA_SERVICE_URL + 'rollback', json=data)
+        action=lambda: crear_compra(data),
+        compensation=lambda: compensar_compra(data["compra_id"]),
     )
-
     # Paso 2: Procesar el pago
     saga_builder.action(
-        action=lambda: requests.post(PAGO_SERVICE_URL, json=data),
-        compensation=lambda: requests.post(PAGO_SERVICE_URL + '/rollback', json=data)
+        action=lambda: crear_pago(data),
+        compensation=lambda: compensar_pago(data["pago_id"]),
     )
-
     # Paso 3: INVENTARIO
     saga_builder.action(
-        action=lambda: requests.post(INVENTARIO_SERVICE_URL, json=data),
-        compensation=lambda: None  # No necesito rollback, es el paso final
+        action=lambda: inventory_sell(data),
+        compensation=lambda: None,
     )
-
+    # Ejecutar la saga
     saga = saga_builder.build()
-
     try:
-    
         saga.execute()
-        print("todo salio bien")
         return {"status": "Orden creada con éxito"}, 200
     except SagaError as e:
         print("algo no anda bien")
---
+++ test/test_saga.py
@@ -0,0 +1,49 @@
+import unittest
+import requests
+from services.orquestador import saga_compra
+from unittest.mock import patch
+
+
+class TestSagaCompra(unittest.TestCase):
+    def test_saga_compra_exito(self):
+
+        requests.post(
+            "http://127.0.0.1:5000/api/v1/stock/refuel",
+            json={"product_id": 1, "quantity": 10},
+        )
+
+        data = {
+            "producto_id": 1,
+            "product_id": 1,
+            "direccion_envio": "Calle Falsa 123",
+            "precio": 100,
+            "medio_pago": "tarjeta",
+            "quantity": 2,
+        }
+
+        response, status_code = saga_compra(data)
+
+        self.assertEqual(status_code, 200)
+        self.assertEqual(response["status"], "Orden creada con éxito")
+
+    def test_saga_inventory_sell_error(self):
+        requests.post(
+            "http://127.0.0.1:5000/api/v1/stock/refuel",
+            json={"product_id": 2, "quantity": 10},
+        )
+        data = {
+            "pago_id": 1,
+            "compra_id": 1,
+            "producto_id": 9,
+            "product_id": 9,
+            "direccion_envio": "Calle Falsa 123",
+            "precio": 100,
+            "medio_pago": "tarjeta",
+            # "quantity": 2,
+        }
+        response, status_code = saga_compra(data)
+        print(response)
+
+
+if __name__ == "__main__":
+    unittest.main()
