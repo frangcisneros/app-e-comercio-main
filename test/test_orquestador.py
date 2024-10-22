bc47215959b0f8eb0bae82faba9ea42c027a6775
Francisco Cisneros <franbichir@gmail.com>
2024-10-22 09:58:19 -0300

    eliminando __pycache__

--- test/test_orquestador.py
+++ test/test_orquestador.py
@@ -3,6 +3,7 @@ import requests
 from services.orquestador import crear_orden
 from unittest.mock import patch
 
+
 class TestCrearOrden(unittest.TestCase):
 
     def test_crear_orden_exito(self):
@@ -14,15 +15,15 @@ class TestCrearOrden(unittest.TestCase):
         self.assertEqual(status_code, 200)
         self.assertEqual(response["status"], "Orden creada con éxito")
 
-    @patch('requests.post')
+    @patch("requests.post")
     def test_crear_orden_fallo(self, mock_post):
         # Configurar el mock para que devuelva un fallo en el servicio de catálogo
         mock_post.side_effect = [
             requests.Response(),  # Respuesta para la acción del catálogo (fallo simulado)
             requests.Response(),  # Respuesta para la acción de compra (si llegara a ejecutarse)
-            requests.Response()   # Respuesta para la acción de pago (si llegara a ejecutarse)
+            requests.Response(),  # Respuesta para la acción de pago (si llegara a ejecutarse)
         ]
-        
+
         # Configuramos un fallo en la primera llamada
         mock_post.return_value.status_code = 500
         mock_post.return_value.json.return_value = {"status": "Error"}
@@ -36,5 +37,5 @@ class TestCrearOrden(unittest.TestCase):
         self.assertIn("Error", response["detail"])
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     unittest.main()
