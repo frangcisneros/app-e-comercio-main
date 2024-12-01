711b01d45728d41db2b8c4d22383014979b061c6
Francisco Cisneros <franbichir@gmail.com>
2024-12-01 11:02:23 -0300

    update env in containers

--- migrations.sh
+++ migrations.sh
@@ -2,11 +2,15 @@
 #EJECUTAR CON TERMINAL GIT BASH
 
 services=("ms_compras" "ms_stock" "ms_catalogo" "ms_pago")
+databases=("main_app_dev_db" "main_app_test_db" "main_app_prod_db")
 
-for service in "${services[@]}"
+for db in "${databases[@]}"
 do
-  echo "Inicializando base de datos para $service"
-  docker exec -it $service flask db init
-  docker exec -it $service flask db migrate
-  docker exec -it $service flask db upgrade
+  echo "Inicializando migraciones para la base de datos $db"
+  docker exec -it db psql -U main_app_user -d $db -c "CREATE TABLE IF NOT EXISTS alembic_version (version_num VARCHAR(32) NOT NULL)"
+  for service in "${services[@]}"
+  do
+    echo "Ejecutando migraciones para $service en $db"
+    docker exec -it $service bash -c "export DATABASE_URL=postgresql+psycopg2://main_app_user:main_app_password@db:5432/$db && flask db upgrade"
+  done
 done
